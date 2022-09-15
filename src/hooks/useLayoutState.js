d23733ea97fef8094ece7e39f819782162c54c1e
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-09-15 14:14:36 +0200

    feat(hooks): add layout context
    Makes it possible to persist global layout state across sessions.
    
    Copied from https://github.com/bpmn-io/properties-panel/blob/main/src/context/LayoutContext.js

---
+++ src/hooks/useLayoutState.js
@@ -0,0 +1,50 @@
+import {
+  useContext,
+  useEffect,
+  useState
+} from 'preact/hooks';
+
+import {
+  LayoutContext
+} from '../context/LayoutContext';
+
+/**
+ * Creates a state that persists in the global LayoutContext.
+ *
+ * @example
+ * ```jsx
+ * function Component(props) {
+ *   const [ open, setOpen ] = useLayoutState([ 'containers', 'foo', 'open' ], false);
+ * }
+ * ```
+ *
+ * @param {(string|number)[]} path
+ * @param {any} [defaultValue]
+ *
+ * @returns {[ any, Function ]}
+ */
+export function useLayoutState(path, defaultValue) {
+  const {
+    getLayoutForKey,
+    setLayoutForKey
+  } = useContext(LayoutContext);
+
+  const layoutForKey = getLayoutForKey(path, defaultValue);
+  const [ value, set ] = useState(layoutForKey);
+
+  // react on global state changes
+  useEffect(() => {
+    set(layoutForKey);
+  }, [ layoutForKey ]);
+
+  const setState = (newValue) => {
+
+    // (1) set component state
+    set(newValue);
+
+    // (2) set context
+    setLayoutForKey(path, newValue);
+  };
+
+  return [ value, setState ];
+}
