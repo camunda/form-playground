5aa9fa6c1a83c8c188b4993e83e85b4b78d2ca45
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-09-15 14:12:44 +0200

    feat(components): add playground component

---
+++ test/spec/components/PlaygroundComponent.spec.js
@@ -0,0 +1,249 @@
+import 'preact/debug';
+
+import TestContainer from 'mocha-test-container-support';
+
+import mitt from 'mitt';
+
+import {
+  render,
+  waitFor
+} from '@testing-library/preact/pure';
+
+import { html } from 'htm/preact';
+
+import {
+  classes as domClasses,
+  query as domQuery
+} from 'min-dom';
+
+import { PlaygroundComponent } from '../../../src/components/PlaygroundComponent';
+
+import {
+  expectNoViolations
+} from '../../TestHelper';
+
+const noop = () => {};
+
+const defaultSchema = {
+  components: [
+    {
+      key: 'creditor',
+      label: 'Creditor',
+      type: 'textfield',
+      description: 'foo',
+      validate: {
+        required: true
+      }
+    }
+  ]
+};
+
+const defaultData = {
+  creditor: 'foo'
+};
+
+
+describe('components/PlaygroundComponent', function() {
+
+  let container;
+
+  beforeEach(function() {
+    container = TestContainer.get(this);
+  });
+
+
+  it('should render', async function() {
+
+    // given
+    let result;
+    await waitFor(() => {
+      result = createPlaygroundComponent({ container });
+    });
+
+    // then
+    expect(domQuery('.cfp-root', result.container)).to.exist;
+  });
+
+
+  it('should open containers (validate)', async function() {
+
+    // given
+    const onInit = (playground) => {
+
+      // skip proper attachments
+      playground._ref.attachPreviewContainer = noop;
+      playground._ref.attachResultContainer = noop;
+      playground.open();
+    };
+    let result;
+    await waitFor(() => {
+      result = createPlaygroundComponent({ container, onInit });
+    });
+
+    // then
+    expect(isOpen(getPanel('form-definition', result.container))).to.be.true;
+    expect(isOpen(getPanel('form-preview', result.container))).to.be.true;
+    expect(isOpen(getPanel('form-input', result.container))).to.be.true;
+    expect(isOpen(getPanel('form-output', result.container))).to.be.true;
+  });
+
+
+  it('should collapse containers (design)', async function() {
+
+    // given
+    const onInit = (playground) => {
+
+      // skip proper attachments
+      playground._ref.attachPreviewContainer = noop;
+      playground._ref.attachResultContainer = noop;
+      playground.open();
+      playground.collapse();
+    };
+    let result;
+    await waitFor(() => {
+      result = createPlaygroundComponent({ container, onInit });
+    });
+
+    // then
+    expect(isOpen(getPanel('form-definition', result.container))).to.be.true;
+    expect(getPanel('form-preview', result.container)).to.not.exist;
+    expect(domQuery('.cfp-collapsed-preview', result.container)).to.exist;
+    expect(isOpen(getPanel('form-input', result.container))).to.be.false;
+    expect(isOpen(getPanel('form-output', result.container))).to.be.false;
+  });
+
+
+  it('should attach components', async function() {
+
+    // given
+    let ref;
+    const paletteSpy = sinon.spy();
+    const editorSpy = sinon.spy();
+    const previewSpy = sinon.spy();
+    const inputSpy = sinon.spy();
+    const outputSpy = sinon.spy();
+    const propertiesPanelSpy = sinon.spy();
+
+    const onInit = (playground) => {
+      playground._ref.attachPaletteContainer = paletteSpy;
+      playground._ref.attachEditorContainer = editorSpy;
+      playground._ref.attachPreviewContainer = previewSpy;
+      playground._ref.attachDataContainer = inputSpy;
+      playground._ref.attachResultContainer = outputSpy;
+      playground._ref.attachPropertiesPanelContainer = propertiesPanelSpy;
+      ref = playground;
+    };
+
+    await waitFor(() => {
+      createPlaygroundComponent({ container, onInit });
+    });
+
+    // when
+    await waitFor(() => {
+      ref.emit('formPlayground.rendered');
+      ref.open();
+    });
+
+    // then
+    expect(paletteSpy).to.have.been.called;
+    expect(editorSpy).to.have.been.called;
+    expect(previewSpy).to.have.been.called;
+    expect(inputSpy).to.have.been.called;
+    expect(outputSpy).to.have.been.called;
+    expect(propertiesPanelSpy).to.have.been.called;
+  });
+
+
+  describe('layout context', function() {
+
+    it('should notify on layout changed', async function() {
+
+      // given
+      const spy = sinon.spy();
+
+      const emitter = mitt();
+
+      let ref;
+      const onInit = (playground) => {
+        ref = playground;
+        playground._ref.attachPreviewContainer = noop;
+        playground._ref.attachResultContainer = noop;
+      };
+
+      await waitFor(() => {
+        createPlaygroundComponent({ container, emitter, onInit });
+      });
+
+      emitter.on('formPlayground.layoutChanged', spy);
+
+      // when
+      await waitFor(() => {
+        ref.open();
+      });
+
+      // then
+      expect(spy).to.have.been.called;
+    });
+
+  });
+
+
+  describe('a11y', function() {
+
+    it('should have no violations', async function() {
+
+      // given
+      this.timeout(5000);
+
+      let node;
+
+      await waitFor(() => {
+        const result = createPlaygroundComponent({ container });
+
+        node = result.container;
+      });
+
+      // then
+      await expectNoViolations(node);
+    });
+
+  });
+
+});
+
+
+// helper //////////////
+
+function createPlaygroundComponent(options = {}) {
+
+  const events = mitt();
+
+  const {
+    container,
+    data = defaultData,
+    emitter = events,
+    onInit = noop,
+    schema = defaultSchema,
+    ...restOptions
+  } = options;
+
+  return render(html`
+    <${PlaygroundComponent} 
+      data=${data}
+      emitter=${emitter}
+      onInit=${onInit}
+      schema=${schema}
+      ...${ restOptions } />`,
+  {
+    container
+  }
+  );
+}
+
+function getPanel(idx, container) {
+  return domQuery(`.cfp-collapsible-panel[data-idx="${idx}"]`, container);
+}
+
+function isOpen(node) {
+  return domClasses(node).contains('open');
+}
