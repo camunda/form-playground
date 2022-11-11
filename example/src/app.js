e0f5ef911ac6f8eb305680765ebfff559a2a2eac
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-11-11 15:18:15 +0100

    chore(example): add empty data and schema

--- example/src/app.js
+++ example/src/app.js
@@ -4,7 +4,7 @@ import {
 
 import download from 'downloadjs';
 
-import schema from '../resources/form.json';
+import schema from '../resources/empty.json';
 
 import './style.css';
 import './light-theme.css';
@@ -15,31 +15,7 @@ const container = document.querySelector('.playground');
 const designBtn = document.querySelector('.design');
 const validateBtn = document.querySelector('.validate');
 
-const data = {
-  creditor: 'John Doe Company',
-  amount: 456,
-  invoiceNumber: 'C-123',
-  approved: true,
-  approvedBy: 'John Doe',
-  mailto: [ 'regional-manager', 'approver' ],
-  product: 'camunda-cloud',
-  queriedDRIs: [
-    {
-      'label': 'John Doe',
-      'value': 'johnDoe'
-    },
-    {
-      'label': 'Anna Bell',
-      'value': 'annaBell'
-    },
-    {
-      'label': 'Nico Togin',
-      'value': 'incognito'
-    }
-  ],
-  tags: [ 'tag1', 'tag2', 'tag3' ],
-  language: 'english'
-};
+const data = {};
 
 let mode;
 
